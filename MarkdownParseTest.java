0e8bb436bf5fb75808b2649216dfcd25c968617d
MelodyyyYin <melodyyin31@gmail.com>
2022-01-28 13:42:55 -0800

    fix 2 ()

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -20,4 +20,30 @@ public class MarkdownParseTest {
 
         assertEquals(List.of("https://something.com", "some-page.html"), links);
     }
+
+    @Test
+    public void inccorectFile() throws IOException {
+        Path fileName = Path.of("incorrect.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+
+        assertEquals(List.of(), links);
+    }
+
+    @Test
+    public void imageFile() throws IOException {
+        Path fileName = Path.of("image.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+
+        assertEquals(List.of("google.com"), links);
+    }
+    @Test
+    public void newFile() throws IOException {
+        Path fileName = Path.of("newfile.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+
+        assertEquals(List.of("https://docs.google.com/document/d/1enL_jhAcSdQ1lHbUpXYqPfW6yy4eBnv5T9iod1aKQoE/edit#heading=h.6htt4v2f3w9w", "google.com/12345()"), links);
+    }
 }
